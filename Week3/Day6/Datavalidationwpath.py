e68f008df08cfcd32ad3f15ca6392c34b2ed1aa7
Vaishnavi <vaishnavishah6311@gmail.com>
2026-04-24 02:57:19 -0700

    Day6

---
+++ Week3/Day6/Datavalidationwpath.py
@@ -0,0 +1,101 @@
+###Data Validation With Path and Query 
+
+from typing import Optional
+from fastapi import FastAPI,Path , Query
+from pydantic import BaseModel,Field
+
+app = FastAPI()
+
+class Book:
+    id : int
+    title : str
+    author : str
+    description : str
+    rating : str
+    published_date : int
+
+    def __init__(self,id,title,author,description,rating,published_date):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+        self.published_date = published_date
+
+
+class BookRquest(BaseModel):
+    id:Optional[int] = Field(title = 'id is not needed')
+    title:str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description : str = Field(min_length=1 , max_length=100)
+    rating: int = Field(gt = 0,lt = 6)
+    published_date : int = Field(gt = 1999 , lt=2031)
+
+    class Config:
+        schema_Extra = {
+            'example' : {
+                'title' : 'A new book',
+                'author' :'Codingwithroby',
+                'description' : 'A new Description of a book',
+                'rating' :5,
+                'published_date' : 2029
+            }
+        }
+
+BOOKS = [
+    Book(1,'Computer Science Pro','codingwithroby','A very nice book !',5,2000),
+    Book(2,'Be Fast with FastAPI','codingwithroby','A great book !',5,2006),
+    Book(3,'Master Endpoints','codingwithroby','A awesome book !',5,2022),
+    Book(4 ,'HP1','Author 1' ,'Book Description',5,2009),
+    Book(5,'HP2','Author 2' , 'Book Description',5,2027),
+    Book(6,'HP3','Author 3','Book Description',5,2028)
+
+]    
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS 
+
+@app.get("/books/{book_id}")
+async def read_book(book_id:int = Path(gt = 0)):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+        
+@app.get("/books")
+async def read_book_by_rating(book_rating :int = Query(gt = 0 , lt = 6)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+
+@app.get("/books/publish")
+async def read_books_by_publish_date(published_date: int = Query(gt = 1999 , lt=2031)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+            return books_to_return
+        
+
+@app.post("/create-book")
+async def create_book(book_request : BookRquest):
+    new_book = Book(**book_request.dict())
+    BOOKS.append(find_book_id(new_book))
+    
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id +1
+    return book
+
+@app.put("/books/update_book")
+async def update_book(book: BookRquest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id:int = Path(gt = 0)):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
