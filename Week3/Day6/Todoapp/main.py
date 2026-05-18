b0698be924ac6b2884c52ee7314f99e758addabe
Vaishnavi <vaishnavishah6311@gmail.com>
2026-05-18 05:28:38 -0700

    todo app

--- Week3/Day6/Todoapp/main.py
+++ Week3/Day6/Todoapp/main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI , Depends , HTTPException
 import models
-from database import engine
+from models import Todos
+from database import engine , sessionlocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind = engine)
+
+def get_db():
+    db = sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependancy =Annotated[Session , Depends(get_db)]
+
+@app.get("/")
+async def read_all(db: db_dependancy):
+    return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}")
+async def read_todo(db:db_dependancy,todo_id:int):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code = 404 , details = 'Todo not found.')
+
